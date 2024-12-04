292e2b4e9027bfc22403b32fe94617c7d716e7d4
Emil Bogren <info@emilbogren.se>
2024-12-04 13:27:07 +0100

    Mull It Over

--- Source/Day03.swift
+++ Source/Day03.swift
@@ -1,5 +1,6 @@
 import Foundation
 import ArgumentParser
+import RegexBuilder
 
 struct Day3: ParsableCommand {
   private var fileURL = Bundle.module.url(
@@ -11,7 +12,33 @@ struct Day3: ParsableCommand {
   mutating func run() throws {
     let input = try String(contentsOf: fileURL, encoding: .utf8)
 
-    print(input)
+    let part1 = solve(input)
+    print(part1)
+
+    let part2 = solve(input, dont: false)
+    print(part2)
+  }
+
+  private func solve(_ input: String, dont: Bool? = nil) -> Int {
+    guard let dont else { return parseAndMultiply(input) }
+    let separator = dont ? "do()" : "don't()"
+    let parts = input.split(separator: separator, maxSplits: 1)
+    if parts.count == 0 { return 0 }
+    if parts.count == 1 { return parseAndMultiply(String(parts[0])) }
+    return dont
+    ? solve(String(parts[1]), dont: false)
+    : parseAndMultiply(String(parts[0])) + solve(String(parts[1]), dont: true)
+  }
+
+  private func parseAndMultiply(_ input: String) -> Int {
+    let regex = Regex {
+      "mul("
+      Capture { OneOrMore(.digit) }
+      ","
+      Capture { OneOrMore(.digit) }
+      ")"
+    }
+    return input.matches(of: regex)
+      .reduce(into: 0) { $0 += Int($1.output.1)! * Int($1.output.2)! }
   }
 }
-
